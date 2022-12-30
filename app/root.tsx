49b73c2dd232251e6ebc76ba0e910d1b38a809f0
demon-monk <antdude@126.com>
2022-12-30 11:17:27 +0800

    add styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -7,6 +7,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import globalStylesUrl from '~/styles/global.css';
+import globalMediumStylesUrl from '~/styles/global-medium.css';
+import globalLargeStylesUrl from '~/styles/global-large.css';
 
 /**
  * This is the root of React
@@ -17,11 +20,26 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: globalStylesUrl },
+  {
+    rel: 'stylesheet',
+    href: globalMediumStylesUrl,
+    media: 'print, (min-width: 640px)',
+  },
+  {
+    rel: 'stylesheet',
+    href: globalLargeStylesUrl,
+    media: 'screen, (min-width: 1024px)',
+  },
+];
+
 export default function App() {
   return (
     <html lang='en'>
       <head>
         <Meta />
+        {/*  Links is used to import all links exported by active routes */}
         <Links />
       </head>
       <body>
--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,4 +1,10 @@
+import type { LinksFunction } from '@remix-run/node';
 import { Outlet } from '@remix-run/react';
+import stylesUrl from '~/styles/jokes.css';
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: stylesUrl },
+];
 
 export default function Jokes() {
   return (
